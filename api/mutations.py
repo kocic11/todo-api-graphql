b395db69fa9a4e6525ba65c099c38eb6aa187ecc
Aleksandar Kocic <25288526+kocic11@users.noreply.github.com>
2020-12-04 09:25:10 -0500

    Removed unused variable

--- api/mutations.py
+++ api/mutations.py
@@ -40,7 +40,7 @@ def resolve_mark_done(obj, info, todo_id):
             "success": True,
             "todo": todo.to_dict()
         }
-    except AttributeError:
+    except AttributeError as error:
         payload = {
             "success": False,
             "errors":  [f"Todo matching id {todo_id} was not found"]
